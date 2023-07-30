d1fd0621d17477f9b6c390cbbf96180be299c70e
Melo <meluszko@gmail.com>
2023-07-30 23:40:45 +0200

    leetcode 49

--- Arrays_and_Hashing/leetcode.py
+++ Arrays_and_Hashing/leetcode.py
@@ -19,16 +19,22 @@ class Solution():
         :type t: str
         :rtype: bool
         """
-        dictionary_s = {}
-        dictionary_t = {}
-        if len(s) != len(t):
-            return False
-        for i in range(len(s)):
-            dictionary_s[s[i]] = dictionary_s.get(s[i], 0) + 1
-            dictionary_t[t[i]] = dictionary_t.get(t[i], 0) + 1
-        if dictionary_s == dictionary_t:
+        # dictionary_s = {}
+        # dictionary_t = {}
+        # if len(s) != len(t):
+        #     return False
+        # for i in range(len(s)):
+        #     dictionary_s[s[i]] = dictionary_s.get(s[i], 0) + 1
+        #     dictionary_t[t[i]] = dictionary_t.get(t[i], 0) + 1
+        # if dictionary_s == dictionary_t:
+        #     return True
+        # return False
+        
+        # simpler way
+        if sorted(s) == sorted(t):
             return True
-        return False
+        else:
+            return False
 
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dictionary = {}
@@ -38,6 +44,16 @@ class Solution():
                 return [dictionary[result], i]
             else:
                 dictionary[nums[i]] = i
-            
-            
-            
+
+    def groupAnagrams(self, strs: List[str]):
+        dictionary = {}
+        for i in strs:
+            letter_count = [0] * 26
+            for j in i:
+                letter_count[ord(j) - ord("a")] += 1
+            if tuple(letter_count) not in dictionary:
+                dictionary[tuple(letter_count)] = [i]
+            else:
+                dictionary[tuple(letter_count)].append(i)
+        return list(dictionary.values())
+               
--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from Arrays_and_Hashing.leetcode import Solution
 nums = [3,2,4]
 target = 6
 s, t = "rat", "car"
-print(Solution.twoSum(nums, target))
+strs = ["eat","tea","tan","ate","nat","bat"]
+print(Solution().groupAnagrams(strs))
