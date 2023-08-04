55fa660680aa0294d29aaca1b6943da510a8f437
melo1901 <meluszko@gmail.com>
2023-08-04 13:56:35 +0200

    125 update

--- Two_pointers/leetcode.py
+++ Two_pointers/leetcode.py
@@ -5,14 +5,8 @@ class Solution():
     # leetcode 125
     def isPalindrome(self, s: str) -> bool:           
         s = re.sub(r'[\W_]', '', s.lower())
-        if s == s[::-1]:
-            return True
-        else:
-            return False
+        return s == s[::-1]
         
         # version 2
         # s = ''.join(filter(str.isalnum, s))      
-        # if s.lower() == s[::-1].lower():
-        #     return True
-        # else:
-        #     return False
+        # return s.lower() == s[::-1].lower()
